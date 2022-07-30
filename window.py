9e4e5c6f3590962cf36d1b08ed8fa62064735630
Julien VIGNAU-ESPINE <julien@cognitive-design-systems.com>
2022-07-30 21:29:09 +0200

    :feat: can take inputs and works but no error handling

--- window.py
+++ window.py
@@ -11,6 +11,14 @@
 # imports
 import tkinter as tk
 from tkinter import ttk
+from convert import *
+import string
+
+def atoi(str):
+    resultant = 0
+    for i in range(len(str)):
+        resultant = resultant * 10 + (ord(str[i]) - ord('0'))        #It is ASCII substraction 
+    return resultant
 
 def create_window(window_name):
     # @brief  : Create a simple tkinter window
@@ -22,14 +30,16 @@ def create_window(window_name):
     window.geometry('600x500+50+50')
     return (window)
 
-def choose_currency(window):
-    # @brief  : Create the combobox to choose the currency to convert
+def choose_currency_and_amount(window):
+    # @brief  : Create the comboboxs to choose the currency 
+    # and the input and convert it
     # @params : take the window in wich the combobox will be displayed
     # @return : return the chosen currencies code
     
     def retrieve():
-        print(currency1.get(), currency2.get())
+        print(convert(currency1.get(), currency2.get(), atoi(amount.get())))
     
+    amount =tk.StringVar(window)
     cur_code_list = ["USD", "EUR", "GBP", "JPY"]
     currency1 = ttk.Combobox(window, values = cur_code_list)
     currency1.set("Pick an Option")
@@ -38,10 +48,9 @@ def choose_currency(window):
     currency2 = ttk.Combobox(window, values = cur_code_list)
     currency2.set("Pick an Option")
     currency2.pack(padx = 5, pady = 5)
-
+    amount_input = tk.Entry (window, textvariable = amount).pack()
     Button = tk.Button(window, text = "Submit", command = retrieve)
     Button.pack(padx = 5, pady = 5)
-
 
 def mainloop():
     window = create_window("Currency Converter")
@@ -49,7 +58,7 @@ def mainloop():
     title.pack()
     question = tk.Label(window, text="\n\nChoose currencies :", font=("Arial", 15))
     question.pack()
-    choose_currency(window)
+    choose_currency_and_amount(window)
     window.mainloop()
 
 mainloop()
